a56f8a936441efc83640d2e2c32bdb7eba0b4bd9
Kadyrberdi <lhojamkulyyev@gmail.com>
2020-08-07 17:22:02 +0300

    deleted some codes in js

--- js/map.js
+++ js/map.js
@@ -1,11 +1,3 @@
-/*function myMap() {
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {
-    center: new google.maps.LatLng(7.8906391, 98.29500014248785),
-    zoom: 15,
-  };
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-}*/
 google.maps.event.addDomListener(window, "load", function () {
   var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 15,
@@ -13,17 +5,14 @@ google.maps.event.addDomListener(window, "load", function () {
     scrollwheel: false,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
   });
-
   var infoWindow = new google.maps.InfoWindow();
 
-  var onMarkerClick = function () {
-    var marker = this;
+  var onMapmouseover = function () {
+    var map = this;
     var latLng = marker.getPosition();
-    infoWindow.setContent(marker.title);
-
-    infoWindow.open(map, marker);
+    infoWindow.open(map);
   };
-  google.maps.event.addListener(map, "click", function () {
+  google.maps.event.addListener(map, "mouseover", function () {
     infoWindow.close();
   });
 
@@ -31,7 +20,6 @@ google.maps.event.addDomListener(window, "load", function () {
     var marker = this;
     var latLng = marker.getPosition();
     infoWindow.setContent(marker.title);
-
     infoWindow.open(map, marker);
   };
   google.maps.event.addListener(map, "mouseover", function () {
@@ -46,6 +34,5 @@ google.maps.event.addDomListener(window, "load", function () {
   });
   // infoWindow.open(map, marker1);
   google.maps.event.addListener(marker1, "mouseover", onMarkermouseover);
-  google.maps.event.addListener(map, "mouseover", onMarkermouseover);
-  google.maps.event.addListener(marker1, "click", onMarkerClick);
+  google.maps.event.addListener(map, "mouseover", onMapmouseover);
 });
