e3cd0b90d873af19070e35506ed250134e1d036e
Kadyrberdi <lhojamkulyyev@gmail.com>
2020-08-08 19:22:03 +0300

    fixed bugs in second check

--- send.php
+++ send.php
@@ -1,11 +1,16 @@
 <?php
+// Файлы phpmailer
 require 'phpmailer/PHPMailer.php';
 require 'phpmailer/SMTP.php';
 require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 $email = $_POST['email'];
+
+// Формирование самого письма
 $title = "Новое обращение Best Tour plan";
 $body = "
 <h2>Новое обращение</h2>
@@ -13,7 +18,10 @@ $body = "
 <b>Телефон:</b> $phone<br><br>
 <b>Сообщение:</b><br>$message
 <b>Подписка:</b><br>$email
+
 ";
+
+// Настройки PHPMailer
 $mail = new PHPMailer\PHPMailer\PHPMailer();
 try {
     $mail->isSMTP();   
@@ -21,21 +29,33 @@ try {
     $mail->SMTPAuth   = true;
     $mail->SMTPDebug = 2;
     $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
-    $mail->Host       = 'ssl://smtp.gmail.com'; 
-    $mail->Username   = 'lhojamkulyyev@gmail.com'; 
-    $mail->Password   = 'LOC27P14'; 
+
+    // Настройки вашей почты
+    $mail->Host       = 'ssl://smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'lhojamkulyyev@gmail.com'; // Логин на почте
+    $mail->Password   = 'LOC27P14'; // Пароль на почте
     $mail->SMTPSecure = 'ssl';
     $mail->Port       = 465;
-    $mail->setFrom('lhojamkulyyev@gmail.com', 'John Mammet'); 
+    $mail->setFrom('lhojamkulyyev@gmail.com', 'John Mammet'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
     $mail->addAddress('knurberdiyev@gmail.com');  
+     // Ещё один, если нужен
+
+   
+// Отправка сообщения
 $mail->isHTML(true);
 $mail->Subject = $title;
 $mail->Body = $body;    
 
+// Проверяем отравленность сообщения
 if ($mail->send()) {$result = "success";} 
 else {$result = "error";}
- catch (Exception $e) {
+
+} catch (Exception $e) {
     $result = "error";
     $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
 }
+
+// Отображение результата
 header("Location: thankyou.html");
